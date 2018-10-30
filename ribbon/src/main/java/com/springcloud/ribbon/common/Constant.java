5f49cd94cd0369ec21eec95b150ccc0cc043bb32
jiangping <jiangping@dimaidt.com>
2018-10-30 14:05:55 +0800

    添加常量配置文件

---
+++ ribbon/src/main/java/com/springcloud/ribbon/common/Constant.java
@@ -0,0 +1,15 @@
+package com.springcloud.ribbon.common;
+
+/**
+ * @author Administrator
+ * @Description: TODO
+ * @date 2018/10/30 13:54
+ */
+public class Constant {
+
+    private static final String RESP_STATUS_SUCCESS = "resp.status.success";
+    private static final String RESP_STATUS_ERROR = "resp.status.error";
+    private static final String RESP_STATUS_REPEAT = "resp.status.repeat";
+    private static final String RESP_STATUS_PARAMSERR = "resp.status.paramsErr";
+
+}
