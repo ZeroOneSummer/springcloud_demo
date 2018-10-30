697431a43272487187aa24416978b69604e6e978
jiangping <jiangping@dimaidt.com>
2018-10-30 14:12:57 +0800

    hibernate引用配置文件参数

--- ribbon/src/main/java/com/springcloud/ribbon/common/Constant.java
+++ ribbon/src/main/java/com/springcloud/ribbon/common/Constant.java
@@ -1,5 +1,10 @@
 package com.springcloud.ribbon.common;
 
+import org.hibernate.validator.resourceloading.PlatformResourceBundleLocator;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 /**
  * @author Administrator
  * @Description: TODO
@@ -7,9 +12,19 @@ package com.springcloud.ribbon.common;
  */
 public class Constant {
 
+    private static final String FILE_NAME = "parmas_config";  //配置文件名
     private static final String RESP_STATUS_SUCCESS = "resp.status.success";
     private static final String RESP_STATUS_ERROR = "resp.status.error";
     private static final String RESP_STATUS_REPEAT = "resp.status.repeat";
     private static final String RESP_STATUS_PARAMSERR = "resp.status.paramsErr";
+
+    //加载配置文件
+    private static ResourceBundle resourceBundle = new PlatformResourceBundleLocator(Constant.FILE_NAME).getResourceBundle(Locale.getDefault());
+
+    public static void main(String[] args) {
+        //获取配置文件变量
+        String success = resourceBundle.getString(Constant.RESP_STATUS_SUCCESS);
+        System.out.println(success);
+    }
 
 }
