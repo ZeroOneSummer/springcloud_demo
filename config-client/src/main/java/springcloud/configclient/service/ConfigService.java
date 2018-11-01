c62523dc41142e4727246d894c02f49843451d05
jiangping <jiangping@dimaidt.com>
2018-11-01 10:47:16 +0800

    分布式配置中心(Spring Cloud Config)结合Eureka注册中心实现热更新，文件仓库用git或者native

--- config-client/src/main/java/springcloud/configclient/service/ConfigService.java
+++ config-client/src/main/java/springcloud/configclient/service/ConfigService.java
@@ -1,6 +1,7 @@
 package springcloud.configclient.service;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2018/10/31 12:00
  */
 @RestController
+@RefreshScope
 public class ConfigService {
 
     @Value("${foo}")
@@ -17,7 +19,7 @@ public class ConfigService {
 
     @RequestMapping(value = "/config")
     public String getConfig(){
-        System.out.println("从git仓库获取配置：" + foo);
+        System.out.println("从git仓库或本地仓库获取配置：" + foo);
         return foo;
     }
 
